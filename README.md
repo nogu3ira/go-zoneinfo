5f40373e5dd95aa7743db4cf2d94241f0be4f902
Adriano Nogueira <nogueirajp@gmail.com>
2019-03-15 04:36:04 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # go-zoneinfo
-Zoneinfo embedded data in Go, helpful for applications running on Windows.
+Zoneinfo embedded data in Go, helpful for applications running on Windows or if you want to load within the application, regardless of the operating system.
+
+
+## Quick Example
+
+#### Download and install in console
+
+    go get github.com/nogu3ira/go-zoneinfo
+
+#### Create file `test.go`
+```go
+package main
+
+import "github.com/nogu3ira/go-zoneinfo"
+
+func main() {
+	tz := "America/Kentucky/Louisville"
+	loc, err := zoneinfo.LoadLocation(tz)
+	if err != nil {
+		println("Problem in", tz, err.Error())
+	} else {
+		println("OK to", tz, loc)
+	}
+}
+```
+#### Build and run
+
+    go build test.go
+
+#### Run in Windows
+
+    test.exe
+
+#### Run in Linux
+
+    ./test
+
+#### Result if successful
+
+    OK to America/Kentucky/Louisville 0xc0000be000
+
+#### Result if error/fail (tz := "America/Kentucky/Loull")
+
+    Problem in LoadLocation America/Kentucky/Loull not found
+
+## Adding to an existing project
+
+Replace in your project all use of:
+```
+    time.LoadLocation(tz)
+```
+To:
+```
+    zoneinfo.LoadLocation(tz)
+```
+
---
+++ zi.go
@@ -0,0 +1,83 @@
+package zoneinfo
+
+import (
+	"bytes"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"time"
+)
+
+func bindataRead(data []byte, name string) ([]byte, error) {
+	gz, err := gzip.NewReader(bytes.NewBuffer(data))
+	if err != nil {
+		return nil, fmt.Errorf("Read %q: %v", name, err)
+	}
+
+	var buf bytes.Buffer
+	_, err = io.Copy(&buf, gz)
+	clErr := gz.Close()
+
+	if err != nil {
+		return nil, fmt.Errorf("Read %q: %v", name, err)
+	}
+	if clErr != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}
+
+type asset struct {
+	bytes []byte
+	info  os.FileInfo
+}
+
+type bindataFileInfo struct {
+	name    string
+	size    int64
+	mode    os.FileMode
+	modTime time.Time
+}
+
+func (fi bindataFileInfo) Name() string {
+	return fi.name
+}
+func (fi bindataFileInfo) Size() int64 {
+	return fi.size
+}
+func (fi bindataFileInfo) Mode() os.FileMode {
+	return fi.mode
+}
+func (fi bindataFileInfo) ModTime() time.Time {
+	return fi.modTime
+}
+func (fi bindataFileInfo) IsDir() bool {
+	return false
+}
+func (fi bindataFileInfo) Sys() interface{} {
+	return nil
+}
+
+//LoadLocation returns the embedded Location with the given name.
+func LoadLocation(name string) (*time.Location, error) {
+	if name == "" || name == "UTC" || name == "Local" {
+		return time.LoadLocation(name)
+	}
+	cannonicalName := strings.Replace(name, "\\", "/", -1)
+	if f, ok := _bindata[cannonicalName]; ok {
+		a, err := f()
+		if err != nil {
+			return nil, fmt.Errorf("LoadLocation %s can't read by error: %v", name, err)
+		}
+
+		loc, err := time.LoadLocationFromTZData(name, a.bytes)
+		if err != nil {
+			return nil, err
+		}
+		return loc, nil
+	}
+	return nil, fmt.Errorf("LoadLocation %s not found", name)
+}
